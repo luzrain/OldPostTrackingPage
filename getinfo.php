d6da0441a48b3a8c1d7c72dfb34ce35eb0171ce1
luzrain <25800964+luzrain@users.noreply.github.com>
2020-05-24 14:19:02 +0300

    Post change api format. Removed index details download due to new weird index api

--- getinfo.php
+++ getinfo.php
@@ -3,68 +3,45 @@
 header('Content-Type: application/json; charset=utf-8');
 header('Cache-Control: no-cache');
 
-$input = json_decode(file_get_contents('php://input'));
+$barcode = isset($_POST['barcode']) ? $_POST['barcode'] : null;
 
 //Функция для http get запроса
 function getData($url, $data, $headers) {
 	$curl = curl_init();
 	curl_setopt_array($curl, [
-		CURLOPT_RETURNTRANSFER	=>	true,
-		CURLOPT_HEADER			=>	false,
-		CURLOPT_CONNECTTIMEOUT	=>	5,
-		CURLOPT_HTTPHEADER		=>	$headers,
-		CURLOPT_URL				=>	$url . '?' . http_build_query($data),
+		CURLOPT_URL				=> $url,
+		CURLOPT_RETURNTRANSFER	=> true,
+		CURLOPT_HEADER			=> false,
+		CURLOPT_CONNECTTIMEOUT	=> 5,
+		CURLOPT_POST			=> true,
+		CURLOPT_POSTFIELDS		=> http_build_query($data),
+		CURLOPT_HTTPHEADER		=> $headers,
+		CURLOPT_SSL_VERIFYPEER	=> false,
 	]);
 	$result = curl_exec($curl);
-	$error = curl_error($curl);
-	$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 	curl_close($curl);
-	
-	if($code != 200) {
-		http_response_code(500);
-		exit;
-	}
-	
 	return $result;
 }
 
-//Вывод информации о почтовом отделении
-if (!empty($input->zipcode)) {
-	
-	$url = 'https://www.pochta.ru/portal-portlet/delegate/postoffice-api/method/offices.find.byCode';
-	$data = [
-		'postalCode' => $input->zipcode,
-	];
-	$headers = [
-		'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0',
-		'Accept: application/json',
-		'X-Requested-With: XMLHttpRequest',
-		'Referer: https://www.pochta.ru/offices',
-		'Cookie: PORTAL_LANGUAGE=ru_RU',
-		'Connection: close',
-	];
-	
-	echo getData($url, $data, $headers);
-	exit;
-	
 //Вывод информации о почтовом отправлении
-} elseif (!empty($input->barcode)) {
+if (!empty($barcode)) {
 	
-	$url = 'https://www.pochta.ru/tracking';
-	$data = [
+	$url = 'https://www.pochta.ru/tracking?' . http_build_query([
 		'p_p_id' => 'trackingPortlet_WAR_portalportlet',
 		'p_p_lifecycle' => '2',
 		'p_p_state' => 'normal',
 		'p_p_mode' => 'view',
-		'p_p_resource_id' => 'getList',
+		'p_p_resource_id' => 'tracking.get-by-barcodes',
 		'p_p_cacheability' => 'cacheLevelPage',
 		'p_p_col_id' => 'column-1',
 		'p_p_col_pos' => '1',
-		'p_p_col_count' => '2',
-		'barcodeList' => $input->barcode,
+		'p_p_col_count' => '1',
+	]);
+	$data = [
+		'barcodes' => $barcode,
 	];
 	$headers = [
-		'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0',
+		'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:70.0) Gecko/20100101 Firefox/76.0',
 		'Accept: application/json',
 		'X-Requested-With: XMLHttpRequest',
 		'Referer: https://www.pochta.ru/tracking',
--- js/core.js
+++ js/core.js
@@ -6,11 +6,6 @@ var xhr;
 
 document.addEventListener('DOMContentLoaded', function() {
 	
-	//Отрисовка всплывающего окна с информацией об отделении
-	var index_float = document.createElement('div');
-	index_float.id = 'zipcode-float';
-	document.getElementsByTagName('body')[0].appendChild(index_float);
-	
 	//Отрисовка выпадающего списка и историей
 	var search_history = document.createElement('div');
 	search_history.id = 'search-history';
@@ -27,43 +22,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById('mem-counter').setAttribute('title', 'Количество сохраненных трек номеров.\nДля вызова истории сделайте двойной клик\nпо полю ввода или нажмите кнопку "вниз".');
 	document.getElementById('mem-counter').innerHTML = saved_barcodes.length;
 	
-	//Наведение мышкой на индекс (для отображения адреса отделения)
-	document.addEventListener('mouseover', function(e) {
-		if(!e.target.classList.contains('index_detail')) return;
-		
-		var zipcode = parseInt(e.target.innerHTML);
-		var zip_data_saved = sessionStorage.getItem('zip-' + zipcode);
-		var x_offset;
-		
-		if(zip_data_saved) {
-			document.getElementById('zipcode-float').innerHTML = zip_data_saved;
-		} else {
-			document.getElementById('zipcode-float').innerHTML = 'Загрузка...';
-			doZipSearch(zipcode, function(zipcode, details) {
-				document.getElementById('zipcode-float').innerHTML = details;
-				sessionStorage.setItem('zip-' + zipcode, details);
-			});
-		}
-		
-		document.getElementById('zipcode-float').style.display = 'block';
-		
-		//Чтобы всплывающая информация об отделении не выходила за экран
-		if ( document.getElementById('zipcode-float').offsetWidth + e.pageX > document.documentElement.clientWidth ) {
-			x_offset = document.documentElement.clientWidth - document.getElementById('zipcode-float').offsetWidth - 20;
-		} else {
-			x_offset = e.pageX + 10;
-		}
-		
-		document.getElementById('zipcode-float').style.top = e.pageY + 15 + 'px';
-		document.getElementById('zipcode-float').style.left = x_offset + 'px';
-	});
-	
-	//Скрытие всплывающего окна с информацией об отделении
-	document.addEventListener('mouseout', function(e) {
-		if(!e.target.classList.contains('index_detail')) return;		
-		document.getElementById('zipcode-float').style.display = 'none';
-	});
-
 	//Нажатие кнопки поиска
 	document.getElementById('button-search').addEventListener('click', function() {
 		if(document.getElementById('barcode-number').value == '') {
@@ -132,7 +90,7 @@ function showTrackDetail(jsondata) {
 	document.getElementById('table_box').style.display = 'block';
 	
 	//Чтение json структуры
-	var TrackData = jsondata.list[0].trackingItem;
+	var TrackData = jsondata.trackingItem;
 	
 	//Заголовок, информация о посылке	
 	document.getElementById('track-title').classList.remove('arrived');
@@ -181,14 +139,6 @@ function showTrackDetail(jsondata) {
 	}
 	
 	document.getElementById('table_result-rows').innerHTML = html;
-	
-	var indexes = document.getElementsByClassName('index');
-	for (var i=0; i<indexes.length; i++) {
-		var index_num = parseInt(indexes[i].innerHTML);
-		if(String(index_num).length == 6) {
-			indexes[i].classList.add('index_detail');
-		}
-	}
 }
 
 //Поиск трек номера
@@ -212,11 +162,12 @@ function doSearch() {
 	
 	xhr = new XMLHttpRequest();
 	xhr.open('POST', 'getinfo.php');
-	xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-	xhr.send(JSON.stringify({barcode: barcode}));
+	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	xhr.send('barcode=' + barcode);
 	xhr.responseType = 'json';
 	
 	xhr.onload = function() {
+		var answer = xhr.response;
 		if (xhr.status != 200) {
 			return showMessage('Ошибка', xhr.status + ' - ' + xhr.statusText);
 		} else {
@@ -226,16 +177,16 @@ function doSearch() {
 			}
 			
 			//Ингода сервер почты возвращает ответ нулевой длины, обрабатываем такой ответ, как ошибку
-			if (xhr.response.list.length == 0) {
+			if (answer.length == 0) {
 				return showMessage('Результат поиска', 'База данных временно недоступна.');
 			}
 			
 			//Если информации о посылке с таким трек-номером нету
-			if (xhr.response.list[0].trackingItem.trackingHistoryItemList.length == 0) {
+			if (answer.response[0].trackingItem.trackingHistoryItemList.length == 0) {
 				return showMessage('Результат поиска', 'Информация о почтовом отправлении ' + barcode + ' не найдена');
 			}
 			
-			return showTrackDetail(xhr.response);
+			return showTrackDetail(answer.response[0]);
 		}
 	};
 	
@@ -243,31 +194,6 @@ function doSearch() {
 		return showMessage('Ошибка', 'Запрос не удался.');
 	};
 }
-
-//Поиск информации о почтовом отделении по индексу
-function doZipSearch(zipcode, cb) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', 'getinfo.php');
-	xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-	xhr.send(JSON.stringify({zipcode: zipcode}));
-	xhr.responseType = 'json';
-	
-	xhr.onload = function() {
-		if (xhr.status != 200) {
-			cb(zipcode, '<span style="color:#e84132;">' + xhr.status + ' - ' + xhr.statusText + '</span>');
-		} else if (xhr.response.office) {
-			let details = '<b>' + xhr.response.office.postalCode + ', ' + xhr.response.office.region + ', ' + xhr.response.office.settlement + '</b><br>Адрес отделения: <b>' + xhr.response.office.addressSource	+ '</b><br>Телефон отделения: <b>+7 (' + xhr.response.office.phones[0].phoneTownCode + ') ' + xhr.response.office.phones[0].phoneNumber + '</b>';
-			cb(zipcode, details);
-		} else {
-			cb(zipcode, 'Нет данных');
-		}
-	};
-	
-	xhr.onerror = function() {
-		cb(zipcode, '<span style="color:#e84132;">Запрос не удался.</span>');
-	};
-}
-
 
 /*================================ Работа с localstorage для хранения последних трек номеров ================================*/
 
